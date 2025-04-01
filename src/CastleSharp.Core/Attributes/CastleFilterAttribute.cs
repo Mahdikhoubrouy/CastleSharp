7f42b9cf22db80da16b6b15b28b58fe34735e0f1
Mahdi Khoubrouy <mahdikhoubrouy@gmail.com>
2025-04-02 02:13:23 +0330

    add summary

--- src/CastleSharp.Core/Attributes/CastleFilterAttribute.cs
+++ src/CastleSharp.Core/Attributes/CastleFilterAttribute.cs
@@ -14,7 +14,16 @@ namespace CastleSharp.Core.Attributes
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, Inherited = true)]
     public class CastleFilterAttribute : Attribute
     {
+        /// <summary>
+        /// An optional list of allowed chat IDs.
+        /// If provided, only updates from these chats will be accepted.
+        /// </summary>
         public long[]? Chats { get; set; }
+
+        /// <summary>
+        /// The name of a method (in the same class) that will be called to evaluate custom logic.
+        /// The method should accept a single <see cref="Update"/> parameter and return a <see cref="bool"/>.
+        /// </summary>
         public string? ConditionName { get; set; }
     }
 }
